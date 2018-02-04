3f91d60a7ee10a5004eb35f5aa631a7f0f300d95
MoonlightOwl <murky.owl@gmail.com>
2018-02-04 05:31:20 +0200

    working file selector

--- src/illuminator.cr
+++ src/illuminator.cr
@@ -11,10 +11,28 @@ module Illuminator
   error 404 do
     render "src/views/404.ecr"
   end
-  get "/" do
+
+  def self.render_index(path, files)
     current_file = files.first
-    log = Log.new(config.get("path") + current_file)
+    log = Log.new(path + current_file)
     render "src/views/index.ecr"
+  end
+
+  get "/index.html" do
+    render_index(config.get("path"), files)
+  end
+  get "/" do
+    render_index(config.get("path"), files)
+  end
+
+  get "/:filename" do |env|
+    current_file = env.params.url["filename"]
+    if File.exists? config.get("path") + current_file
+      log = Log.new(config.get("path") + current_file)
+      render "src/views/index.ecr"
+    else
+      render "src/views/404.ecr"
+    end
   end
 
   Kemal.config.port = config.get("port").to_i
