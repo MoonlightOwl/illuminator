030352955ab176cd8db738b59941fbbefb180f2b
MoonlightOwl <murky.owl@gmail.com>
2018-02-04 03:17:24 +0200

    load and show actual list of log files

--- src/config.cr
+++ src/config.cr
@@ -7,7 +7,6 @@ module Illuminator
       lines = File.read_lines(path)
       fields = lines.map { |line| line.split(':') }.transpose
       @hash = Hash.zip(fields[0], fields[1])
-      puts @hash
     end
 
     def get(key)
--- src/illuminator.cr
+++ src/illuminator.cr
@@ -5,10 +5,13 @@ require "kemal"
 module Illuminator
   config = Config.new "./config/illuminator.conf"
 
+  files = Dir.children(config.get("path")).sort.reverse
+
   error 404 do
     render "src/views/404.ecr"
   end
   get "/" do
+    current_file = files.first
     render "src/views/index.ecr"
   end
 
